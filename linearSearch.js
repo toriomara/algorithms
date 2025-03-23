606380ef5d252b707e4d981ffb4775e0aadb20f9
Vladimir Budkevich <budkevich.vladimir@gmail.com>
2025-03-23 10:22:41 +0300

    fix: correct linearSearch

--- linearSearch.js
+++ linearSearch.js
@@ -1,17 +1,17 @@
 // Linear search - O(n)
 
 const array = [1, 4, 5, 8, 5, 1, 2, 7, 5, 2, 11];
-count = 0;
 
 function linearSearch(array, item) {
-  for (let i = 0; i < array.length; i++ ) {
-    count +=1;
+  let count = 0;
+  for (let i = 0; i < array.length; i++) {
+    count += 1;
     if (array[i] === item) {
       return i;
     }
   }
-  return null
+  return null;
 }
 
 console.log(linearSearch(array, 8));
-console.log('count = ', count);
+console.log("Count: ", count);
